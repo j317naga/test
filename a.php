9e866031d672474885626b101ba6be7308ecb7ee
j317naga <j520317m@mails.cc.ehime-u.ac.jp>
2024-06-26 10:43:53 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php //foreach ループ内で条件をチェックする
+  foreach(file('/usr/share/dict/words') as $line){　// strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示する
+    if (strpos($line, 'xy') !== false) {
+      echo "$line\n";
+    }
+  } 
+?>
